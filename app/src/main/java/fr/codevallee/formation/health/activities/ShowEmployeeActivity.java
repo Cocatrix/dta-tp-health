045ec4d21b32853f84614f9adb8a18a34fd8955e
Cocatrix <cocatrix@laposte.net>
2017-10-20 09:47:36 +0200

    Pager beginning

---
+++ app/src/main/java/fr/codevallee/formation/health/activities/ShowEmployeeActivity.java
@@ -0,0 +1,15 @@
+package fr.codevallee.formation.health.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.codevallee.formation.health.R;
+
+public class ShowEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_employee);
+    }
+}
